2d95ed806410d7a0a1f56807c7eb17e456f7d570
kawasaki03 <h.kawasaki03@gmail.com>
2020-10-30 20:07:36 +0900

    チャットルーム名の表示と、チャットルーム削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:edit,:update]
   get 'messages/index'
   root to: "rooms#index"
-  resources :rooms,only: [:new,:create] do
+  resources :rooms,only: [:new,:create,:destroy] do
   resources :messages,only: [:new,:create,:index]
   end
 end
