7513f415efb97244043e154a9577e0a68690ee9b
kawasaki03 <h.kawasaki03@gmail.com>
2021-01-22 21:10:08 +0900

    テーブルの変更

--- db/migrate/20210120141245_create_student_records.rb
+++ db/migrate/20210120141245_create_student_records.rb
@@ -2,7 +2,7 @@ class CreateStudentRecords < ActiveRecord::Migration[6.0]
   def change
     create_table :student_records do |t|
       t.string :title
-      t.string :note
+      t.text :note
       t.references :student,foreign_key: true
       t.references :teacher,foreign_key: true
       t.timestamps
