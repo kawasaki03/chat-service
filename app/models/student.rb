b3782b7e357fce728cf51f1fbb90689dbefd7b33
kawasaki03 <h.kawasaki03@gmail.com>
2021-02-20 22:55:44 +0900

    ユーザー削除機能の実装

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,13 +5,13 @@ class Student < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
   has_many :room_students
-  has_many :rooms, through: :room_students
+  has_many :rooms, through: :room_students,dependent: :destroy
   has_many :messages
   belongs_to :teacher
-  has_many :teacher_rooms
-  has_one_attached :image
-  validates :status_message, length: { maximum: 50 }
-  has_many :student_records
+  has_many :teacher_rooms,dependent: :destroy
+  has_one_attached :image,dependent: :destroy
+  validates :status_message,length: { maximum: 50 }
+  has_many :student_records,dependent: :destroy
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i.freeze
   validates_format_of :password, on: :create, with: PASSWORD_REGEX, message: 'は半角の英字・数字を両方含んでください'
